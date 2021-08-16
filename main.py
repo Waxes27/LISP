29fa172b8fc0a184557db187c002519a4a3d383c
wtc <wtc@pop-os.localdomain>
2021-08-16 17:32:07 +0200

    Delegate

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from functions import Atom
 from functions import List
 
+operators = ['+','-','*','/']
+
 def evaluator(command):
     if '(' not in command and ')' not in command:
         Atom.Unit(command)
     else:
+        
         List.List(command)
 
 
