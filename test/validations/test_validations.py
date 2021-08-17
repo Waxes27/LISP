5a0c5bee233a9b39d43e95945db8f46becbc451b
bdubeJHB <bdube@student.wethinkcode.co.za>
2021-08-17 15:18:56 +0200

    Fixed the tests import issue. Also implemented the basic functionality for checking quote commands

--- test/validations/test_validations.py
+++ test/validations/test_validations.py
@@ -1,5 +1,5 @@
 import unittest
-from validation import validations
+from validation.validations import Validations
 
 class ValidationTests(unittest.TestCase):
     def test_brackets_always_evaluated_properly(self):
@@ -10,13 +10,13 @@ class ValidationTests(unittest.TestCase):
         invalid_exp_4 = "(((("
         invalid_exp_5 = "(+ (5 (+ 14 1))"
 
-        self.assertTrue(validations.isValidExpressionBrackets(valid_exp_2))
-        self.assertTrue(validations.isValidExpressionBrackets(valid_exp_6))
-        self.assertTrue(validations.isValidExpressionBrackets(valid_exp_8))
-        self.assertFalse(validations.isValidExpressionBrackets(invalid_exp_3))
-        self.assertFalse(validations.isValidExpressionBrackets(invalid_exp_4))
-        self.assertFalse(validations.isValidExpressionBrackets(invalid_exp_5))
-        self.assertFalse(validations.isValidExpressionBrackets("("))
+        self.assertTrue(Validations.isValidExpressionBrackets(valid_exp_2))
+        self.assertTrue(Validations.isValidExpressionBrackets(valid_exp_6))
+        self.assertTrue(Validations.isValidExpressionBrackets(valid_exp_8))
+        self.assertFalse(Validations.isValidExpressionBrackets(invalid_exp_3))
+        self.assertFalse(Validations.isValidExpressionBrackets(invalid_exp_4))
+        self.assertFalse(Validations.isValidExpressionBrackets(invalid_exp_5))
+        self.assertFalse(Validations.isValidExpressionBrackets("("))
 
     def test_quote_detected_properly(self):
         """Check if quote-only commands are consistently detected"""
@@ -25,10 +25,10 @@ class ValidationTests(unittest.TestCase):
         invalid_quote_1 = "1"
         invalid_quote_brackets = "(quot wtc)"
 
-        self.assertTrue(validations.isValidQuote(valid_quote_1))
-        self.assertTrue(validations.isValidQuote(valid_quote_brackets))
-        self.assertFalse(validations.isValidQuote(invalid_quote_1))
-        self.assertFalse(validations.isValidQuote(invalid_quote_brackets))
+        self.assertTrue(Validations.isValidQuote(valid_quote_1))
+        self.assertTrue(Validations.isValidQuote(valid_quote_brackets))
+        self.assertFalse(Validations.isValidQuote(invalid_quote_1))
+        self.assertFalse(Validations.isValidQuote(invalid_quote_brackets))
 
 
 if __name__ == "__main__":
