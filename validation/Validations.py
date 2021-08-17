eda117f746dff895613a2bce08bfc66f98d0eb5e
bdubeJHB <bdube@student.wethinkcode.co.za>
2021-08-17 09:55:31 +0200

    Added a little bit of groundwork for project specs

---
+++ validation/Validations.py
@@ -0,0 +1,8 @@
+class Validations:
+    def brackets(command) -> bool:
+        """Checks if the brackets in an expression are all properly closed"""
+        #TODO: Implement logic for bracket count
+
+    def literal(command) -> bool:
+        """Checks if the command is a quote that should be printed as is"""
+        #TODO: Implement logic for quote detection
