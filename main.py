fbe8274ec16ea36d78589b1cb6e578f735b77930
Waxes28 <nceba.dumasi@gmail.com>
2021-08-18 16:16:58 +0200

    Functions __init__.py edited

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from functions import List
 from functions import cons
 
 operators = ['+','-','*','/']
+list_of_commands = ['atom','quote','car','cdr','eq','cond','lambda','label','defunc']
 
 def evaluator(command):
     if '(' not in command and ')' not in command:
