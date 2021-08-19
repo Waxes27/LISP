d41a115b17b753f1a925e7f46e0c64d84053e855
anmugwab <anmugwab@student.wethinkcode.co.za>
2021-08-19 20:01:48 +0200

    fixed code

--- functions/cdr.py
+++ functions/cdr.py
@@ -1,9 +1,10 @@
 class Cdr:
     def __init__(self,command):
-        # if self.lcount(command):
-        self.do(command)
-        # else:
-            # print('Unexpected amount of Parenthesis...')
+        self.value = ''
+        if self.lcount(command):
+            self.value = self.do(command)
+        else:
+            print('Unexpected amount of Parenthesis...')
 
 
     def lcount(self, command):
@@ -13,38 +14,9 @@ class Cdr:
             return False
 
     def do(self, command):
-        mylist = list(command.replace("'","").replace(' ', ""))
-        return mylist[1:]
-        
-    def dont(self, command):
-        temp = []
-        cdrCount = 0
-        openBrace = False
-        tempString = ""
-        command = command.replace("("," ( ").replace(")", " ) ")
-        
-        n = 0
-
-        while (command[n:].find("cdr") > 0):
-            cdrCount += 1
-
-            n =  command[n:].find("cdr") 
-
-            for x in command[n:]:
-                if (x == "("):
-                    openBrace = True
-                    continue
-                if (x == ")"):
-                    openBrace = False
-                    #tempString += ")"
-                    continue
-                if (openBrace == True):
-                    tempString += x
-                
-                if (x == "cdr"):
-                    break
-            for x in tempString.strip().split(' '):
-                temp.append(x)
-            tempString = ""
-        print(temp[cdrCount:])
-        return temp[cdrCount:]
+        mylist = command.replace("'","")
+        return mylist.split(" ")[1:]
+    
+    
+    def __iter__(self):
+        return iter(self.value)
