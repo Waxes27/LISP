9084db64e9f79550c193b09fdd9fe24928fc1cc6
tlmokabo <tlmokabo@student.wethinkcode.co.za>
2021-08-19 22:39:21 +0200

    Eq basic

--- functions/eq.py
+++ functions/eq.py
@@ -1,6 +1,10 @@
 class Eq:
 
     def checkEqual(self, expression):
+        """
+        this is the basic functionality that checks if the first expression is
+        equal to the second expression.
+        """
         if expression[0] == expression[1]:
             return True
         else:
@@ -8,5 +12,9 @@ class Eq:
 
 
     def equalList(self, expression):
+        """
+        this function is supposed to look for an empty list and return false 
+        if it finds it
+        """
         if '()' in expression:
             return False
